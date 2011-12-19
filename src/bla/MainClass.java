1dfdd056ecbd48fbc95cc8ffb6fe2c10442baf81
Roland Winkler <roland.winkler@gmail.com>
2011-12-19 13:18:42 +0100

    init repository

---
+++ src/bla/MainClass.java
@@ -0,0 +1,21 @@
+package bla;
+
+import blub.TestClass;
+/**
+ * TODO File Description
+ */
+
+/**
+ * TODO Class Description
+ *
+ * @author Roland Winkler
+ */
+public class MainClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Git");
+		
+		System.out.println(new TestClass().test());
+	}
+}
---
+++ src/blub/TestClass.java
@@ -0,0 +1,14 @@
+/**
+ * TODO File Description
+ */
+package blub;
+
+/**
+ * TODO Class Description
+ *
+ * @author Roland Winkler
+ */
+public class TestClass
+{
+	public String test(){return "test";}
+}
